506e94ea44d24252e67072d262e6df1e75611e4b
chonkov <georgi.chonkov27@gmail.com>
2024-04-15 19:29:51 +0300

    Created a simple ERC20 contract - LRT(Liquid Restaking Token), which is to be deployed by a manager

---
+++ contracts/LiquidRestakingToken.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+error LiquidRestakingToken__NotVault(address sender);
+
+contract LiquidRestakingToken is Ownable, ERC20 {
+    // name: Liquid Restaked Ether, symbol: rstETH
+
+    constructor(string memory name_, string memory symbol_) Ownable(msg.sender) ERC20(name_, symbol_) {}
+
+    function mint(address account, uint256 value) external onlyOwner {
+        _mint(account, value);
+    }
+
+    function burn(address account, uint256 value) external onlyOwner {
+        _burn(account, value);
+    }
+}
