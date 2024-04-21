abb9552c3a95ea0c91f450c1a3108a725895c90d
chonkov <georgi.chonkov27@gmail.com>
2024-04-21 20:27:00 +0300

    Entry point for simulation script

---
+++ scripts/deploySystem.js
@@ -0,0 +1,43 @@
+const { ethers } = require("hardhat");
+
+const {
+  WETHAddr,
+  poolAddr,
+  strategyManagerAddr,
+  delegationManagerAddr,
+  operatorAddr,
+  strategyAddr,
+  stETHAddr,
+} = require("./constants.js");
+
+const { getStakedEther } = require("./getStakedEther");
+const { depositStETH } = require("./depositStETH");
+const {
+  deployLiquidityRestakingManager,
+} = require("./deployLiquidityRestakingManager");
+const { delegateTo } = require("./delegateTo");
+const { queueWithdrawal } = require("./queueWithdrawal");
+
+async function main() {
+  const [user] = await ethers.getSigners();
+
+  await getStakedEther(user, WETHAddr, poolAddr);
+
+  const LiquidRestakingManager = await deployLiquidityRestakingManager(
+    stETHAddr,
+    strategyAddr,
+    strategyManagerAddr,
+    delegationManagerAddr
+  );
+
+  await depositStETH(user, LiquidRestakingManager.target, stETHAddr);
+
+  await delegateTo(LiquidRestakingManager, operatorAddr);
+
+  await queueWithdrawal(user, LiquidRestakingManager, strategyAddr);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
